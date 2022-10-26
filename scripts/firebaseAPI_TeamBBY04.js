3aaee1039dc60e3df5332868662cb60bd5c999a9
Matthew Siggs <mcsiggs@shaw.ca>
2022-10-26 10:35:18 -0700

    Added functionality to login page, added firebase authentication data to authentication.js

--- scripts/firebaseAPI_TeamBBY04.js
+++ scripts/firebaseAPI_TeamBBY04.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyBncmjmFmezcMEeHpksv15wrBsBXBXqgCU",
+    authDomain: "comp1800-bby04-2022.firebaseapp.com",
+    projectId: "comp1800-bby04-2022",
+    storageBucket: "comp1800-bby04-2022.appspot.com",
+    messagingSenderId: "833252751523",
+    appId: "1:833252751523:web:84ce621388e391e1bce716"
 };
 
 //--------------------------------------------
