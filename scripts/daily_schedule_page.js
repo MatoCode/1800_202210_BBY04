32f7f2c6be08b6fe64f307e760dc0b18565be0fd
Lijuan Zhu <lisazhuv@gmail.com>
2022-11-02 08:49:03 -0700

    fix bugs in daily_schedule_page

--- scripts/daily_schedule_page.js
+++ scripts/daily_schedule_page.js
@@ -1,11 +1,11 @@
 document.getElementsByClassName("day_schedule")[1].style.display = "none";
 function show0() {
-    document.getElementsByClassName("day_schedule")[0].style.display = "block";
+    document.getElementsByClassName("day_schedule")[0].style.display = "";
     document.getElementsByClassName("day_schedule")[1].style.display = "none";
 }
 function show1() {
     document.getElementsByClassName("day_schedule")[0].style.display = "none";
-    document.getElementsByClassName("day_schedule")[1].style.display = "block";
+    document.getElementsByClassName("day_schedule")[1].style.display = "";
 }
 
 
@@ -20,7 +20,7 @@ for (let i = 0; i < 24; i++) {
     str_day1 += "<tr><td><p class=\"hour_line_odd\">" + i + ":00" + "</p></td>" + "<td>" + "\
     <button class=\"hour_button\" type=\"button\" id=\"button" + i + "_day1\" onclick=\"changeSchedule(id)\">make a schedule</button>" + "</td></tr>";
 }
-document.querySelector('#schedule_table_body_day1').innerHTML = str;
+document.querySelector('#schedule_table_body_day1').innerHTML = str_day1;
 function changeSchedule(button_id){
     element=document.getElementById(button_id);
     element.style.backgroundColor = "lightblue";  
@@ -38,11 +38,11 @@ var days_in_months = 32 - new Date(date.getFullYear(), date.getMonth(), 32).getD
 // }
 
 
-$("#date0-goes-here").text((date.getDate() - week -1) % days_in_months + 1);
-$("#date1-goes-here").text((date.getDate() - week ) % days_in_months + 1);
-$("#date2-goes-here").text((date.getDate() - week +1 ) % days_in_months +1 );
-$("#date3-goes-here").text((date.getDate() - week +2 ) % days_in_months + 1);
-$("#date4-goes-here").text((date.getDate() - week +3 ) % days_in_months +1 );
-$("#date5-goes-here").text((date.getDate() - week +4 ) % days_in_months + 1);
-$("#date6-goes-here").text((date.getDate() - week +5 ) % days_in_months + 1);
+$("#date0-goes-here").text((date.getDate() - week -1 + days_in_months) % days_in_months + 1);
+$("#date1-goes-here").text((date.getDate() - week + days_in_months) % days_in_months + 1);
+$("#date2-goes-here").text((date.getDate() - week +1 + days_in_months) % days_in_months +1 );
+$("#date3-goes-here").text((date.getDate() - week +2 + days_in_months ) % days_in_months + 1);
+$("#date4-goes-here").text((date.getDate() - week +3 + days_in_months ) % days_in_months +1 );
+$("#date5-goes-here").text((date.getDate() - week +4 + days_in_months ) % days_in_months + 1);
+$("#date6-goes-here").text((date.getDate() - week +5 + days_in_months ) % days_in_months + 1);
 
