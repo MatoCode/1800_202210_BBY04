66e3d1d93637e95d95c7026873265a0f9b0315a2
Nelsonph11 <npetersonhui@my.bcit.ca>
2022-11-08 19:10:27 -0800

    needed to commit before I pull anything

--- scripts/messaging.js
+++ scripts/messaging.js
@@ -2,8 +2,6 @@ var messages = document.getElementById("messages")
 var textbox = document.getElementById("textbox")
 var button = document.getElementById("button")
 
-
-
 button.addEventListener("click", function() {
     var newMessage = document.createElement("ul")
     newMessage.innerHTML = textbox.value;
@@ -11,6 +9,3 @@ button.addEventListener("click", function() {
     textbox.value = "";
 });
 
-//cell.addEventListener("click", function creat() {
-//    window.location= 'daily_schedule_page.html';
-//}, false);
