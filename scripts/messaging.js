d40ebab64cc2c5a6c251213b84db0496d20174a0
Nelsonph11 <npetersonhui@my.bcit.ca>
2022-11-08 20:34:40 -0800

    messages now go into firestore db. however, the set of messages from one user creates an ID in the groups section when its supposed to be a part of an already existing group ID. Also, now added user names next to text when sent

--- scripts/messaging.js
+++ scripts/messaging.js
@@ -2,10 +2,31 @@ var messages = document.getElementById("messages")
 var textbox = document.getElementById("textbox")
 var button = document.getElementById("button")
 
+
+//takes text input and saves it to firebase
+//HOW DO I PUT THE TEXT INTO THE GROUP ID ON FIREBASE???? must be with line 17, because it sure isn't user.uid
 button.addEventListener("click", function() {
     var newMessage = document.createElement("ul")
+
+    const db = firebase.firestore();
+    user = firebase.auth().currentUser;
+
     newMessage.innerHTML = textbox.value;
+
+    messageId = textbox.value;
+    db.collection("groups").doc(user.uid).collection("messaging").doc(messageId).set({         //what is uid fro groups???
+        message: document.getElementById('textbox').value,
+    }).then(function() {
+        console.log("New message added to firestore");
+        window.close();
+    }).catch(function (error) {
+        console.log("Error adding new event: " + error);
+    });
+
+    //displays user name and appends to text
+    const textnode = document.createTextNode(user.displayName);
     messages.appendChild(newMessage);
+    messages.appendChild(textnode);
     textbox.value = "";
 });
 
